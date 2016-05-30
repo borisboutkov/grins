0acdeaae4edfa4988ad3c382853f3d04c5f5b826
Paul T. Bauman <ptbauman@gmail.com>
2016-05-30 16:44:46 -0400

    Update tolerance for parallel cases

--- test/regression/hot_cylinder.sh
+++ test/regression/hot_cylinder.sh
@@ -7,4 +7,4 @@ DATA="${GRINS_TEST_DATA_DIR}/hot_cylinder.xdr"
 
 PETSC_OPTIONS="-pc_type asm -pc_asm_overlap 2 -sub_pc_factor_levels 4 -sub_pc_factor_shift_type nonzero"
 
-${LIBMESH_RUN:-} $PROG $PETSC_OPTIONS input=$INPUT soln-data=$DATA vars='Ux Uy p T' norms='L2 H1' tol='8.0e-9'
+${LIBMESH_RUN:-} $PROG $PETSC_OPTIONS input=$INPUT soln-data=$DATA vars='Ux Uy p T' norms='L2 H1' tol='2.0e-7'
